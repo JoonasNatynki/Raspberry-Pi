cf216eb0764727af773a84c14cf8210162eca777
Katulobotomia <joonas.natynki@gmail.com>
2017-02-06 14:49:22 +0200

    .

--- nodejs/natynki_rest_api/html/gpiosbuttons.js
+++ nodejs/natynki_rest_api/html/gpiosbuttons.js
@@ -1,13 +1,25 @@
+let http = request("http");
+
 let openbutton = document.getElementById("opengpios").addEventListener("click", openGPIOs);
 let closebutton = document.getElementById("closegpios").addEventListener("click", closeGPIOs);
 
 
 function openGPIOs()
 {
-    window.location = "opengpios";
+    var options = 
+    {
+        host: 'natynki.net',
+        path: '/opengpios'
+    };
+
 }
 
 function closeGPIOs()
 {
-        window.location = "closegpios";
+    var options = 
+    {
+        host: 'natynki.net',
+        path: '/closegpios'
+    };
+    http.request(options).end();
 }
