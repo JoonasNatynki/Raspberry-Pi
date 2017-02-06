533e070eafb29e61bbd88394700d63c6e88e0b25
Katulobotomia <joonas.natynki@gmail.com>
2017-02-06 14:13:55 +0200

    .

--- nodejs/natynki_rest_api/html/gpiosbuttons.js
+++ nodejs/natynki_rest_api/html/gpiosbuttons.js
@@ -1,6 +1,8 @@
 let openbutton = document.getElementById("opengpios").addEventListener("click", openGPIOs);
 let closebutton = document.getElementById("closegpios").addEventListener("click", closeGPIOs);
 
+var xhttp = new XMLHttpRequest();
+
 function openGPIOs()
 {
     xhttp.open("GET", "opengpios", true);
