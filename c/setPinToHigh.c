14a0a900e03aad8885a60658e94b0ad7116d21ea
Katulobotomia <joonas.natynki@gmail.com>
2017-02-07 19:54:15 +0200

    General pin program

---
+++ c/setPinToHigh.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char * argv[])
+{
+  FILE * file;
+  
+  file = fopen("/sys/class/gpio/gpio" + argv[1] + "/value", "w+");
+  fwrite("1", 1, sizeof("1"), file);
+  fclose(file);
+  return 0;
+}
