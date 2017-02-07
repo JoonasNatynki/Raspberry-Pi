6cf6e5f098fc175f986f6ee655bd4c418fa2c89f
Katulobotomia <joonas.natynki@gmail.com>
2017-02-07 21:25:15 +0200

    .

--- c/setPinToLow.c
+++ c/setPinToLow.c
@@ -9,8 +9,6 @@ int main(int argc, char * argv[])
 
   sprintf(buffer, "/sys/class/gpio/gpio%s/value", atoi(argv[1]));
 
-  // TARKASTA ETTÄ NUMEROT ON 1-9 ja 10-99 EIKÄ 01, 02, 03 jne....
-
   file = fopen(buffer, "w+");
   fwrite("0", 1, sizeof("0"), file);
   fclose(file);
