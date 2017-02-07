0e92377a32268e94a717ed4e02b62c0cc825aa1e
Katulobotomia <joonas.natynki@gmail.com>
2017-02-07 19:56:52 +0200

    .

--- c/setPinToHigh.c
+++ c/setPinToHigh.c
@@ -5,7 +5,7 @@ int main(int argc, char * argv[])
 {
   FILE * file;
   
-  file = fopen("/sys/class/gpio/gpio" + &argv[1] + "/value", "w+");
+  file = fopen("/sys/class/gpio/gpio" + *argv[1] + "/value", "w+");
   fwrite("1", 1, sizeof("1"), file);
   fclose(file);
   return 0;
