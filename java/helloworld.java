8748eb65d11a69995bd8b66bca8582db2357ebcd
Katulobotomia <joonas.natynki@gmail.com>
2017-02-08 19:45:52 +0200

    .

--- java/helloworld.java
+++ java/helloworld.java
@@ -6,17 +6,17 @@ public class helloworld {
     public static void main (String args[]) throws Exception {
 
        String regex = "[ \t]+";
-       FileReader fr = new FileReader (args[0]);        
-       BufferedReader br = new BufferedReader (fr);
+       FileReader fr = new FileReader (args[0]);    // Read the file given as an argument
+       BufferedReader br = new BufferedReader (fr); // Put the content of the file to bufferreader
        String text = new String();
        for (String line; (line = br.readLine()) != null; text += line);
        int count = 0;
        String []parts = text.split(regex);
-          for(String w : parts)
-          {
-	      System.out.println(w);
-	      count++;        
-          }
+       for(String w : parts)
+        {
+           System.out.println(w);
+           count++;
+        }
        System.out.println("Words counted: " + count);
     }
 }
