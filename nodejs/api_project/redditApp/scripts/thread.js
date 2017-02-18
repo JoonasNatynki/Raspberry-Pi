90e6093b7f491aa9d3c7edccb1e7d6eb9afb1ed5
Katulobotomia-läbäri <joonas.natynki@gmail.com>
2017-02-19 00:59:14 +0200

    .

--- nodejs/api_project/redditApp/scripts/thread.js
+++ nodejs/api_project/redditApp/scripts/thread.js
@@ -1,36 +1,96 @@
+var threadghosts = document.getElementsByClassName("threadghost");
 var threads = document.getElementsByClassName("thread");
 var iterationTime = 85;
+var threadsMoving = 0;
 
 
-function loopThreadsAndAnimate(threads)
+function pageInitAnimation()
 {
-    for(i = 0; i < threads.length; i++)
+    console.log("mmmm");
+    for(i = 0; i < threadghosts.length; i++)
     {
-        threadAnimationInit(threads, i);
+        threadAnimationInit(threadghosts, i);
     }
 }
 
-function threadAnimationInit(threads, index)
+// Runs only once when the page is loaded
+function threadAnimationInit(elements, index)
 {
     setTimeout(function()
+        {
+            if(elements[index].style.WebkitAnimationPlayState != "running")
+            {
+                elements[index].addEventListener("animationstart", threadAnimationStart);
+                elements[index].addEventListener("animationend", threadAnimationEnd);
+            }
+            
+        }, 500 + iterationTime * (Math.random() * (5 - 1) + 1));
+}
+
+function rearrangeThreads(threads, index)
+{
+     setTimeout(function()
         {
             if(threads[index].style.WebkitAnimationPlayState != "running")
             {
                 threads[index].style.WebkitAnimationPlayState  = "running";
+                threadsMoving++;
+                threads[index].addEventListener("animationend", threadAnimationEnd);
             }
     }, 500 + iterationTime * (Math.random() * (5 - 1) + 1));
 }
 
-function newThread()
+function threadAnimationEnd()
 {
-    loopThreadsAndAnimate(threads);
-    var thread = document.createElement("div");
-    var threadfield = document.getElementById("threadfield");
-    var i = threadfield.children.length;
-    if(i > 20) return;
-    thread.appendChild(document.createTextNode("Message "+(i - 1)));
-    thread.className = "thread";
-    threadfield.insertBefore(thread, threadfield.firstChild);
-    setTimeout(newThread, 200);
+    threadsMoving--;
+    this.style.WebkitAnimationPlayState  = "paused";
 }
-loopThreadsAndAnimate(threads);
+
+function threadAnimationStart()
+{
+    threadsMoving++;
+    this.style.WebkitAnimationPlayState  = "running";
+}
+
+function makeNewThread()
+{
+    var newthreadghost = document.createElement("div");
+    var newthread = document.createElement("div");
+    var threadfield = document.getElementById("threadfield");
+
+    newthread.id = "thread " + threads.length;
+    newthreadghost.appendChild(document.createTextNode("Thread ghost "+ threadghosts.length));
+    newthread.appendChild(document.createTextNode("Thread "+(i)));
+    newthreadghost.className = "threadghost";
+    newthread.className = "thread";
+
+    threadfield.insertBefore(newthreadghost, threadfield.firstChild);
+}
+
+function test()
+{
+    var tween = new Tweenable();
+    tween.tween(
+                    {
+                        from: { x: 0,  y: 50  },
+                        to:   { x:510, y: 530 },
+                        duration: 500,
+                        easing: 'easeOutQuad',
+                        start: function () { console.log('Off I go!'); },
+                        finish: function () { console.log('And I\'m done!'); }
+                    });
+    console.log(tween);
+    
+    for(i = 0; i < threads.length; i++)
+    {
+        rearrangeThreads(threads, i);
+    }
+}
+
+for(i = 0; i < 5; i++)
+{
+    makeNewThread();
+}
+pageInitAnimation();
+setTimeout(makeNewThread, 1000);
+setTimeout(test, 2000);
