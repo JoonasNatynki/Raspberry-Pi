5c42610e047761c886f25bc49f36a4b81c130a17
Katulobotomia <joonas.natynki@gmail.com>
2017-02-20 15:22:22 +0200

    working animations now

--- nodejs/api_project/redditApp/scripts/thread.js
+++ nodejs/api_project/redditApp/scripts/thread.js
@@ -5,36 +5,37 @@ var threadsMoving = 0;
 
 
 
-function rearrangeThreads()
+function rearrangeThreads(threadsArray, index)
 {
     var threadlen = threadsArray.length;
 
-    for(index = 0; index < threadlen; index++)
-    {
-        var text = getOffsetLeft(threadsArray[index]);
-        console.log(text);
-        console.log(threadsArray.length);
-        console.log(index);
-        
-        var elem = threadsArray[index]; // Element to animate
-        var rect = document.getElementById("threadghost " + index).getBoundingClientRect(); // Current position of said element
-        var bodyRect = document.body.getBoundingClientRect();
-        var threadfield = document.getElementById("threadfield").getBoundingClientRect();
-        var coordx = rect.left - bodyRect.left - threadfield.left;
-        var coordy = rect.top - bodyRect.top - threadfield.top;
+        console.log(iterationTime * (Math.random() * 5) + 1);
+        setTimeout(function ()
+        {
+            var offsetcoords = getCoords(threadghostsArray[index]);
+            threadsArray[index].style.top = 0;
+            
+            var elem = threadsArray[index]; // Element to animate
+            var rect = document.getElementById("threadghost " + index).getBoundingClientRect(); // Current position of said element
+            var bodyRect = document.body.getBoundingClientRect();
+            var threadfield = document.getElementById("threadfield").getBoundingClientRect();
+            var coordx = rect.left - bodyRect.left - threadfield.left;
+            var coordy = offsetcoords.top;
 
-        var trans = Morf.transition(elem, {
-                // New CSS state
-                                            '-webkit-transform': 'translate3d(' + coordx + 'px, ' + coordy + 'px, 0)',
-                                        }, {
-                                            duration: '500ms',
-                                            timingFunction: 'swingFromTo',
-                                            callback: function (elem) {
-                                                // You can optionally add a callback option for when the animation completes.
-                                            }
-                                        });
-    }
+            var trans = Morf.transition(elem, {
+                    // New CSS state
+                                                '-webkit-transform': 'translate3d(' + coordx + 'px, ' + coordy + 'px, 0)',
+                                            }, {
+                                                duration: '500ms',
+                                                timingFunction: 'swingFromTo',
+                                                callback: function (elem) {
+                                                    // You can optionally add a callback option for when the animation completes.
+                                                }
+                                            });
+        }, iterationTime * (Math.random() * 3) + 1);
 }
+
+
 
 function fetchthreads()
 {
@@ -73,27 +74,40 @@ function makeNewThread()
 
     threadfield.insertBefore(newthreadghost, threadfield.firstChild);
     threadfield.insertBefore(newthread, threadfield.firstChild);
-    rearrangeThreads();
+    rearrangeThreadsInit();
 }
 
-function getOffsetLeft( elem )
+function getCoords(elem) { // crossbrowser version
+    var box = elem.getBoundingClientRect();
+
+    var body = document.body;
+    var docEl = document.documentElement;
+
+    var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+    var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+    var clientTop = docEl.clientTop || body.clientTop || 0;
+    var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+    var top  = box.top +  scrollTop - clientTop;
+    var left = box.left + scrollLeft - clientLeft;
+
+    return { top: Math.round(top), left: Math.round(left) };
+}
+
+function rearrangeThreadsInit()
 {
-    var offsetLeft = 0;
-    do {
-      if ( !isNaN( elem.offsetLeft ) )
-      {
-          offsetLeft += elem.offsetLeft;
-      }
-    } while( elem = elem.offsetParent );
-    return offsetLeft;
+    for(var index = 0; index < threadsArray.length; index++)
+    {
+        rearrangeThreads(threadsArray, index);
+    }
 }
-
 
 // Already existing threads...""
-for(i = 0; i < 3; i++)
+for(i = 0; i < 55; i++)
 {
     fetchthreads();
 }
 
 //setInterval(makeNewThread, 8000);
-rearrangeThreads();
+setInterval(makeNewThread, 1000);
