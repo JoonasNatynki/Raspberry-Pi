0a4f9ed124253113371b86a93f04e364f3e6cfda
Katulobotomia <joonas.natynki@gmail.com>
2017-03-06 02:45:19 +0200

    OAuth

---
+++ nodejs/api_project/redditApp/scripts/authorizebutton.js
@@ -0,0 +1,36 @@
+var authorizebutton = document.getElementById("authorizebutton");
+
+authorizebutton.addEventListener("click", onButtonClick);
+
+function onButtonClick()
+{
+    
+    var codes = 
+    {
+        error: function(){console.log("error");},
+        code: "tissit",
+        state: "tissit"
+    }
+/*
+    var tokendata = 
+    {
+        grant_type: "authorization_code",
+        code: codes,
+        redirect_uri: "http://natynki.net/authorize_callback"	
+    }
+
+    var token = $.ajax(
+    {
+        type: "POST",
+        url: "https://www.reddit.com/api/v1/access_token",
+        beforeSend: function (xhr) 
+        {
+            xhr.setRequestHeader ("Authorization", "Basic " + btoa("oz4I-8h8nyfXcg" + ":" + "Nzf6R_2jbnHd59fS8-v4V1UDrNc"));
+        },
+        data: tokendata
+    })
+
+    console.log(token);*/
+
+    window.location = "https://www.reddit.com/api/v1/authorize.compact?client_id=oz4I-8h8nyfXcg&response_type=code&state=tissit&redirect_uri=http://natynki.net/authorize_callback&duration=temporary&scope=identity"
+}
