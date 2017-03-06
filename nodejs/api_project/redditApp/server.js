1263bcf142f4ee8c411e9b0973988405e6d06988
Katulobotomia <joonas.natynki@gmail.com>
2017-03-06 22:18:49 +0000

    jquery install

--- nodejs/api_project/redditApp/server.js
+++ nodejs/api_project/redditApp/server.js
@@ -4,7 +4,7 @@ var express = require("express");
 var app = express();
 var portnumber = 8000;
 var cp = require("child_process");
-var $ = require('jQuery');
+var $ = require('jquery');
 
 console.log("Initialization done.");
 
