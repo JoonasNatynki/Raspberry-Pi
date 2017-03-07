03e97fe2202cd6556558c6ae3b7dc11bd76211ee
Katulobotomia <joonas.natynki@gmail.com>
2017-03-07 09:00:10 +0200

    .

--- nodejs/api_project/redditApp/scripts/authorizebutton.js
+++ nodejs/api_project/redditApp/scripts/authorizebutton.js
@@ -5,7 +5,7 @@ authorizebutton.addEventListener("click", onButtonClick);
 function onButtonClick()
 {
     
-    window.location = "http://natynki.net/reddit/authenticate";
+    window.open("http://natynki.net/reddit/authenticate");
 
     //window.open("https://www.reddit.com/api/v1/authorize?client_id=oz4I-8h8nyfXcg&response_type=code&state=tissit&redirect_uri=http://88.112.159.13:999/authorize_callback&duration=temporary&scope=identity");
     
