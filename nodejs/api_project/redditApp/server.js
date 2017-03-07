98758ecc9899081442773e8ca89f4eae52deb4ca
Katulobotomia <joonas.natynki@gmail.com>
2017-03-07 09:47:49 +0200

    .

--- nodejs/api_project/redditApp/server.js
+++ nodejs/api_project/redditApp/server.js
@@ -23,7 +23,7 @@ app.get("/", function(request, response)
 app.get("/authorize_callback*", function(request, response)
 	{
 		console.log(request.query.code);
-		response.redirect("/");
+		response.render("/", {code: "Code is " + response.query.code});
 	});
 
 app.get("/*", function(request, response)
