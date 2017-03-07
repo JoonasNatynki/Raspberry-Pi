02989b1e208e92461b69768d67a8abfd1220ed83
Katulobotomia <joonas.natynki@gmail.com>
2017-03-07 08:32:21 +0200

    .

--- nodejs/api_project/redditApp/scripts/authorizebutton.js
+++ nodejs/api_project/redditApp/scripts/authorizebutton.js
@@ -6,9 +6,9 @@ function onButtonClick()
 {
     
 
-    window.open("https://www.reddit.com/api/v1/authorize?client_id=oz4I-8h8nyfXcg&response_type=code&state=tissit&redirect_uri=http://88.112.159.13:999/authorize_callback&duration=temporary&scope=identity");
+    var win = window.open("https://www.reddit.com/api/v1/authorize?client_id=oz4I-8h8nyfXcg&response_type=code&state=tissit&redirect_uri=http://88.112.159.13:999/authorize_callback&duration=temporary&scope=identity");
     
-    var hh = document.location.hash;
+    var hh = win.document.location.hash;
 
     console.log(hh);
 
