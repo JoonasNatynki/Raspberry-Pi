568fd8651e4c8b97486cab688fa31660b9e772d4
Katulobotomia <joonas.natynki@gmail.com>
2017-03-07 10:18:01 +0200

    .

--- nodejs/api_project/redditApp/scripts/authorizebutton.js
+++ nodejs/api_project/redditApp/scripts/authorizebutton.js
@@ -6,8 +6,7 @@ function onButtonClick()
 {
     
     window.open("https://www.reddit.com/api/v1/authorize?client_id=oz4I-8h8nyfXcg&response_type=code&state=tissit&redirect_uri=http://88.112.159.13:999/authorize_callback&duration=temporary&scope=identity");
-    
-    
+        
     /*
     var codes = 
     {
--- nodejs/api_project/redditApp/server.js
+++ nodejs/api_project/redditApp/server.js
@@ -23,6 +23,7 @@ app.get("/", function(request, response)
 app.get("/authorize_callback*", function(request, response)
 	{
 		console.log(request.query.code);
+		response.redirect("/");
 	});
 
 app.get("/*", function(request, response)
