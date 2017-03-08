153bd0a94ef7e868ea7b7f87710bbf3339cce09b
Katulobotomia <joonas.natynki@gmail.com>
2017-03-08 20:54:28 +0200

    .

--- nodejs/api_project/redditApp/scripts/frontpageinit.js
+++ nodejs/api_project/redditApp/scripts/frontpageinit.js
@@ -127,7 +127,7 @@ function getUserFrontPage()
                     {
                         console.log("Everything went fine and now rendering subscribed threads!");
                         // Rendering the user info now
-                         makeNewThread(response.responseJSON.children[20].data.title);
+                         makeNewThread(response.responseJSON.data.children[20].data.title);
                     }
                 });
     console.log(response);    
