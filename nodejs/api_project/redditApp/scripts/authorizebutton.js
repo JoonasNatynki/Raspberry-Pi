e1024ed370a512272cf7b21c449aadd44e6f6456
Katulobotomia <joonas.natynki@gmail.com>
2017-03-08 20:32:26 +0200

    .

--- nodejs/api_project/redditApp/scripts/authorizebutton.js
+++ nodejs/api_project/redditApp/scripts/authorizebutton.js
@@ -5,6 +5,6 @@ var authorizationcode; // The auth code
 
 function onButtonClick()
 {
-    var scopes = "mysubreddits identity edit";
+    var scopes = "mysubreddits identity edit read";
     window.open("https://www.reddit.com/api/v1/authorize?client_id=oz4I-8h8nyfXcg&response_type=code&state=tissit&redirect_uri=http://88.112.159.13:999/authorize_callback&duration=permanent&scope=" + scopes);
 }
