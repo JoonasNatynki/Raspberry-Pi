dccd30fae536e79c62e9b9db28c40fa65e9b0271
Katulobotomia <joonas.natynki@gmail.com>
2017-03-08 20:19:39 +0200

    .

--- nodejs/api_project/redditApp/scripts/authorizebutton.js
+++ nodejs/api_project/redditApp/scripts/authorizebutton.js
@@ -6,6 +6,5 @@ var authorizationcode; // The auth code
 function onButtonClick()
 {
     var scopes = "edit";
-    console.log("Opening reddit");
-    //window.open("https://www.reddit.com/api/v1/authorize?client_id=oz4I-8h8nyfXcg&response_type=code&state=tissit&redirect_uri=http://88.112.159.13:999/authorize_callback&duration=permanent&scope=" + scopes);
+    window.open("https://www.reddit.com/api/v1/authorize?client_id=oz4I-8h8nyfXcg&response_type=code&state=tissit&redirect_uri=http://88.112.159.13:999/authorize_callback&duration=permanent&scope=" + scopes);
 }
