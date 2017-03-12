16ac59c82d8a12a162440b07a85cc36e172f9dee
Katulobotomia <joonas.natynki@gmail.com>
2017-03-12 22:06:23 +0200

    .

--- nodejs/api_project/redditApp/scripts/frontpageinit.js
+++ nodejs/api_project/redditApp/scripts/frontpageinit.js
@@ -31,7 +31,7 @@ $("#inputmessage").keypress(function(event)
 				}
 		}); 
         $("#inputmessage").val("");
-        console.log(threadsArray[1].childNodes.getElementByClassName("threadtext").innerHTML);
+        console.log(threadsArray[1].childNodes.getElementsByClassName("threadtext")[0].innerHTML);
     }
 });
 //##############################################################
