84d855bd7c72c49fd7a8e6462f7bedf32813a917
Katulobotomia <joonas.natynki@gmail.com>
2017-03-13 03:19:37 +0200

    .

--- nodejs/api_project/redditApp/scripts/thread.js
+++ nodejs/api_project/redditApp/scripts/thread.js
@@ -185,7 +185,7 @@ function initPage()
     //##############################################################
     // When new comment is submitted and you press ENTER
     document.getElementById("postbutton").addEventListener("click", postMessage);
-    $("#joonas").keyup(function(event)
+    $("#joonas").keypress(function(event)
     {
         if(event.keyCode == 13)
         {
