6acde7ab0247e1b05ef24892fe71dd9aee3028c4
Katulobotomia <joonas.natynki@gmail.com>
2019-07-17 19:36:52 +0000

    Compiling and dynamic linking example done

---
+++ c/CompilingAndLinkingTest/MyFunc.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void MyFunc()
+{
+	std::cout << "Joonas on paska!\n";
+}
---
+++ c/CompilingAndLinkingTest/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "MyFunc.h"
+
+int main(int argc, char* argv[])
+{
+	MyFunc();
+	return 0;
+}
+
